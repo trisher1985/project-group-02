c4adf7398ac95cecf3bc8cdff01d82cdf8651e97
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-04-28 23:44:47 +0300

    update html file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import initializeProjects from './js/my-projects';
 import { setupEmailInput, sendContactRequest } from './js/work-together';
 
 document.addEventListener('DOMContentLoaded', setupEmailInput);
 document.addEventListener('DOMContentLoaded', sendContactRequest);
 
-
-
-import "./js/faq";
-import "./js/reviews";
+import './js/faq';
+import './js/reviews';
+import initializeProjects from './js/my-projects';
